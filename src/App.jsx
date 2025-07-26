f0c850353ca294d2142512d246153c85c9acc81e
dagm <dagimyibabe19@gmail.com>
2025-07-26 17:02:26 +0300

    ger random image and use it as background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-
+import { useState,useEffect } from "react"
 
 function App() {
- 
+    return (
+        <main className="w-screen h-screen flex items-center justify-center">
+            <div
+                className="container w-lg h-72 border-2 border-amber-300 rounded-2xl shadow-lg "
+                style={{
+                    backgroundImage: `url(https://picsum.photos/600/400
+)`,
+                }}
+            ></div>
+        </main>
+    );
 }
 
 export default App
